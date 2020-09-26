3cdb6c8939957c6939609878913794c0ca21a936
pastrychefgamer <52508868+pastrychefgamer@users.noreply.github.com>
2020-09-26 16:28:21 -0700

    code for getting printouts

--- singleton-pattern/firstUse.js
+++ singleton-pattern/firstUse.js
@@ -2,5 +2,7 @@ import FancyLogger from './fancylogger';
 const logger = new FancyLogger();
 
 export default function logFirstImplementation() {
-    
+    logger.printLogCount();
+    logger.log('First File');
+    logger.printLogCount();
 }
--- singleton-pattern/secondUse.js
+++ singleton-pattern/secondUse.js
@@ -2,5 +2,7 @@ import FancyLogger from './fancylogger';
 const logger = new FancyLogger();
 
 export default function logSecondImplementation() {
-    
+    logger.printLogCount();
+    logger.log('Second File');
+    logger.printLogCount();
 }
